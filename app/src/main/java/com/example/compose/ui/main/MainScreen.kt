aaaf209487421553a8a5ca590f44c20bfbb9bb28
Rafal Gurzkowski <rafal@getmimo.com>
2021-05-19 16:49:56 +0200

    Add list with items

--- app/src/main/java/com/example/compose/ui/main/MainScreen.kt
+++ app/src/main/java/com/example/compose/ui/main/MainScreen.kt
@@ -1,19 +1,44 @@
 package com.example.compose.ui.main
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose.ui.theme.ComposeTheme
 
 @Composable
 fun MainScreen() {
     Surface(color = MaterialTheme.colors.background) {
         ComposeTheme {
-            Text(text = "Main Screen", modifier = Modifier.padding(16.dp, 16.dp))
+            ItemList()
         }
     }
+}
+
+@Composable
+private fun ItemList() {
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(60) {
+            Text(text = "Item", fontSize = 24.sp)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    ItemList()
 }
